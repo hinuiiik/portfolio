6df36d063dc108ff89ef4605b5199238158df817
Vikram <github@vknc.mozmail.com>
2025-02-21 21:42:43 -0500

    Fix columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,27 +40,27 @@ export default function Home() {
 
             <ScrollIndicator/>
 
-            <div className="border-t-2 w-[90%] mx-auto mb-11">
-                <p className={"py-10 text-center text-4xl font-raleway"}>Projects</p>
-                <div className={"flex flex-col lg:flex-row items-center justify-center  gap-6"}>
+            <div className="border-t-2 w-[95%] mx-auto mb-11">
+                <p className="py-10 text-center text-4xl font-raleway">Projects</p>
+                <div className="flex flex-wrap justify-center gap-6">
                     <ProjectBox
                         name="Where the Heel - HackNC 2024"
                         image="/wheretheheel_small_compressed.png"
-                        description="Where the Heel was a fun game inspired by GeoGuessr. Each round, you are random images from around campus, and your goal is to pinpoint where it was taken the best you can. Built for the HackNC competition at UNC in 2024 by Vikram Krishnakumar, Jason Armbruster, Colin Volpe, and Owen Ingram."
+                        description="Where the Heel was a fun game inspired by GeoGuessr..."
                         alt="Where the Heel webpage"
                         github="https://github.com/Awesomeness7/HackNC-24"
                     />
                     <ProjectBox
                         name="Chamaquest - Hack_NCState 2025"
                         image="/chamaquest_small.png"
-                        description="ChamaQuest was a project built for the Hack_NCState hackathon in 2025 by Vikram Krishnakumar, Anna Lynch, Colin Volpe, and May Yamanaka. The prototype aims to be a way to connect Rotating Savings and Credit Associations (ROSCAs) in developing countries with banks in order to provide institutional security, while keeping the social aspect many ROSCAs provide."
+                        description="ChamaQuest was a project built for the Hack_NCState hackathon in 2025..."
                         alt="Chamaquest Hackathon Project Page"
                         github="https://github.com/hinuiiik/chamaquest"
                     />
                     <ProjectBox
                         name="The Carolina Razor"
                         image="/carolinarazor_small.png"
-                        description="The Carolina Razor was a concept newspaper for UNC aimed at providing left-wing opinions in a digestible format. The website was created by Vikram Krishnakumar and Alex Kreidler."
+                        description="The Carolina Razor was a concept newspaper for UNC..."
                         alt="Carolina Razor Webpage"
                         github="https://github.com/CarolinaRazor/website"
                     />
